dc5443b9134e82883eb0617873e0984f5b1e8baa
Sree Vidya Lakshmi Kuravi <vidyakuravi@gmail.com>
2026-05-22 17:45:55 +0530

    Added exception handling

--- Week 2/Projects/day_8_student_mgmt_sys.py
+++ Week 2/Projects/day_8_student_mgmt_sys.py
@@ -126,129 +126,135 @@ def save_data(path, d):
 # print(save_data(path, d))
 
 
+try:
+    while True:
+        path = r'E:\Vidya Career\IT JOB\Repati Kosam\Week 2\Projects\day_8_students.txt'
 
-while True:
-    path = r'E:\Vidya Career\IT JOB\Repati Kosam\Week 2\Projects\day_8_students.txt'
+        d = get_stud_data(path)
 
-    d = get_stud_data(path)
+        print("--------------------------------------------- STUDENT MANAGEMENT SYSTEM ---------------------------------------------")
 
-    print("--------------------------------------------- STUDENT MANAGEMENT SYSTEM ---------------------------------------------")
+        print("""
+        1. View all students
+        2. Add student
+        3. Search student
+        4. Update student marks
+        5. Delete student
+        6. Calculate average marks
+        7. Find topper
+        8. Save data
+        9. Exit""")
 
-    print("""
-    1. View all students
-    2. Add student
-    3. Search student
-    4. Update student marks
-    5. Delete student
-    6. Calculate average marks
-    7. Find topper
-    8. Save data
-    9. Exit""")
+        try:
+            ch = int(input("Enter your choice (1 - 9): "))
 
-    ch = int(input("Enter your choice (1 - 9): "))
-
-    if ch == 9:
-        break
-
-
-    elif ch == 1:
-        print("-----------------")
-        print("Student Name || Marks")
-        print("-----------------")
-
-        for i in d:
-            print(i['name'], "=", i['marks'])
+            if ch == 9:
+                break
 
 
-    elif ch == 2:
-        name = input("Enter the name of student: ").strip()
-        s = search_stud(d, name)
+            elif ch == 1:
+                print("-----------------")
+                print("Student Name || Marks")
+                print("-----------------")
 
-        if not s:
-            marks = input(f"Enter the marks of {name}: ").strip()
-            student = add_stud(path, name, marks)
-            print(f"Student with name '{student[0]}' and marks '{student[1]}' has been added successfully")
-        else:
-            print(f"Student with name '{name}' already exists !!!")
-            print("Try again with another name")
-
-        
-    elif ch == 3:
-        name = input("Enter the name of the student you need to search for: ").strip()
-
-        stud = search_stud(d, name)
-        if stud:
-            print('Name : ', stud['name'], '\n' + 'Marks : ', stud['marks'])
-        else:
-            print(f"No such student with name '{name}' found")
+                for i in d:
+                    print(i['name'], "=", i['marks'])
 
 
-    elif ch == 4:
-        name = input("Enter the student name: ")
-        s = search_stud(d, name)
+            elif ch == 2:
+                try:
+                    name = input("Enter the name of student: ").strip()
+                    s = search_stud(d, name)
 
-        if s:
-            marks = input(f"Enter the marks of {name}: ").strip()
-            stud = update_marks(path, name, marks)
-            print(f"{stud['name']}'s marks updated to marks '{marks}' successfully")
-        else:
-            print(f"Student with name: {name} doesnot exist")
-            print('Try again')
+                    if not s:
+                        marks = input(f"Enter the marks of {name}: ").strip()
+                        student = add_stud(path, name, marks)
+                        print(f"Student with name '{student[0]}' and marks '{student[1]}' has been added successfully")
+                    else:
+                        print(f"Student with name '{name}' already exists !!!")
+                        print("Try again with another name")
 
-        
-    elif ch == 5:
-        name = input("Enter the student name you want to delete: ")
-        s = search_stud(d, name)
-        if s:
-            stud = delete_student(path, name)
-            print(f"Student with name {name} has been deleted successfully")
-        else:
-            print(f"Student with name {name} does not exist")
+                except ValueError:
+                    print("The marks must be integer value only.")
 
-        
-    elif ch == 6:
-        if d:
-            avg = average_marks(d)
-            print(f"The average marks of the class is: {avg}")
-        else:
-            print("No student data found")
+                
+            elif ch == 3:
+                name = input("Enter the name of the student you need to search for: ").strip()
+
+                stud = search_stud(d, name)
+                if stud:
+                    print('Name : ', stud['name'], '\n' + 'Marks : ', stud['marks'])
+                else:
+                    print(f"No such student with name '{name}' found")
 
 
-    elif ch == 7:
-        ts = max_marks(d)
-        students = get_name_by_marks(d, ts)
+            elif ch == 4:
+                name = input("Enter the student name: ")
+                s = search_stud(d, name)
 
-        if students:
-            if len(students) == 1:
-                print("The topper of the class is: ")
-                print("Name: ", students[0]['name'])
-                print("Marks: ", students[0]['marks'])
+                if s:
+                    marks = input(f"Enter the marks of {name}: ").strip()
+                    stud = update_marks(path, name, marks)
+                    print(f"{stud['name']}'s marks updated to marks '{marks}' successfully")
+                else:
+                    print(f"Student with name: {name} doesnot exist")
+                    print('Try again')
+
+                
+            elif ch == 5:
+                name = input("Enter the student name you want to delete: ")
+                s = search_stud(d, name)
+                if s:
+                    stud = delete_student(path, name)
+                    print(f"Student with name {name} has been deleted successfully")
+                else:
+                    print(f"Student with name {name} does not exist")
+
+                
+            elif ch == 6:
+                if d:
+                    avg = average_marks(d)
+                    print(f"The average marks of the class is: {avg}")
+                else:
+                    print("No student data found")
+
+
+            elif ch == 7:
+                ts = max_marks(d)
+                students = get_name_by_marks(d, ts)
+
+                if students:
+                    if len(students) == 1:
+                        print("The topper of the class is: ")
+                        print("Name: ", students[0]['name'])
+                        print("Marks: ", students[0]['marks'])
+
+                    else:
+                        print("The toppers of the class are: ")
+
+                        for i in students:
+                            print("-----------------")
+                            print("Name: ", i['name'])
+                            print("Marks: ", i['marks'])
+                else:
+                    print('No data found !!!!')
+
+
+            elif ch == 8:
+                if d:
+                    data = save_data(path, d)
+                    print("The data has been saved successfully in the file !!!")
+
+                else:
+                    print("No data is found")
+
 
             else:
-                print("The toppers of the class are: ")
-
-                for i in students:
-                    print("-----------------")
-                    print("Name: ", i['name'])
-                    print("Marks: ", i['marks'])
-        else:
-            print('No data found !!!!')
-
-    elif ch == 8:
-        if d:
-            data = save_data(path, d)
-            print("The data has been saved successfully in the file !!!")
-
-        else:
-            print("No data is found")
+                print("Wrong Choice :(")
 
 
-    else:
-        print("Wrong Choice :(")
+        except ValueError:
+            print("The choice must be in integer only")
 
-
-
-
-
-
-
+finally:
+    print("The program has been terminated successfully.")
