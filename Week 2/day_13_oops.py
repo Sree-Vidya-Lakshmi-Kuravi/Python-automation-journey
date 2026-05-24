b7f6853c2cea4f1b0c467c7c24b39f2fed2a830e
Sree Vidya Lakshmi Kuravi <vidyakuravi@gmail.com>
2026-05-24 19:40:06 +0530

    Practiced OOPS concept using Encapsulation

--- Week 2/day_13_oops.py
+++ Week 2/day_13_oops.py
@@ -17,7 +17,39 @@ class Manager(Employee):
         print("Employee Name:", self.name)
         print("Employee Salary:", self.salary)
         print("Employee Department:", self.dept)
-        
 
-m1 = Manager("bvns", 140000, "Hi-Tech Enterprise")
-m1.display_info()      
+
+# m1 = Manager("bvns", 140000, "Hi-Tech Enterprise")
+# m1.display_info()    
+
+
+## Bank Account
+
+class BankAccount:
+    
+    def __init__(self, name, balance):
+        self.name = name
+        self.__balance = balance
+
+    def deposit(self, d_amt):
+        self.__balance += d_amt
+        print("Deposited Amount:", d_amt)
+        # print(f"Balance:", self.__balance)
+
+    def withdraw(self, w_amt):
+        if w_amt > self.__balance:
+            print("Insufficient Balance")
+        else:
+            self.__balance -= w_amt
+            print("Withdrawal Amount:", w_amt)
+            # print(f"Balance:", self.__balance)
+
+    def check_balance(self):
+        print("Account Holder Name:", self.name)
+        print("Account Balance:", self.__balance)
+
+b1 = BankAccount("BVNS", 150000)
+# b1.check_balance()
+# b1.deposit(5000)
+b1.withdraw(4500)
+b1.check_balance()
