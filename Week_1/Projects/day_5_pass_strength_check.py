41916de7492169049475b82fae11eb2ac630cdbd
Kuravi Sree Vidya Lakshmi <vidyakuravi@gmail.com>
2026-05-26 23:13:07 +0530

    Implemented basic Password Strength Checker

--- Week_1/Projects/day_5_pass_strength_check.py
+++ Week_1/Projects/day_5_pass_strength_check.py
@@ -7,24 +7,24 @@
 # special character
 # minimum length
 
-# import string 
-# password = '22HM1A0569#siri'
+import string 
+password = '22HM1A0569#siri'
 
-# upp = string.ascii_uppercase
-# low = string.ascii_lowercase
-# spl = string.punctuation
-# num = string.digits
+upp = string.ascii_uppercase
+low = string.ascii_lowercase
+spl = string.punctuation
+num = string.digits
 
-# min_len = True if len(password) >= 8 else False
-# upp_l = any(p in password for p in upp)
-# low_l = any(p in password for p in low)
-# spl_c = any(p in password for p in spl)
-# num_c = any(p in password for p in num)
+min_len = True if len(password) >= 8 else False
+upp_l = any(p in password for p in upp)
+low_l = any(p in password for p in low)
+spl_c = any(p in password for p in spl)
+num_c = any(p in password for p in num)
 
-# if min_len and upp_l and low_l and spl_c and num_c:
-#     print("Strong Password")
-# else:
-#     print("Weak password")
+if min_len and upp_l and low_l and spl_c and num_c:
+    print("Strong Password")
+else:
+    print("Weak password")
 
 
 pw = '22HM1A0569#siri'
