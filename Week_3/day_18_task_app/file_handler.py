d484f5577a801f22f19c5ef3c20f787b092f7728
Kuravi Sree Vidya Lakshmi <vidyakuravi@gmail.com>
2026-05-29 20:34:53 +0530

    Working on task management project

---
+++ Week_3/day_18_task_app/file_handler.py
@@ -0,0 +1,23 @@
+import json
+
+path = r'E:\Vidya Career\IT JOB\Repati Kosam\Week_3\day_18_tasks_app\tasks.json'
+
+# get data
+def get_data(path):
+    try:
+        with open(path) as file:
+            tasks = json.load(file)
+            return tasks
+    except FileNotFoundError:
+        print("No such file found")
+
+# print(get_data(path))
+
+
+# save data
+def save_data(path, data):
+    try:
+        pass
+    except Exception as e:
+        print('Something went wrong')
+
