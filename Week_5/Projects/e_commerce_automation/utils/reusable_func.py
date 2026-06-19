08a12cdc8f00bbf524d0752c1e44450eb0bb1769
Sree Vidya Lakshmi Kuravi <vidyakuravi@gmail.com>
2026-06-19 17:51:44 +0530

    Optimized the utils/reusable_func.py

--- Week_5/Projects/e_commerce_automation/utils/reusable_func.py
+++ Week_5/Projects/e_commerce_automation/utils/reusable_func.py
@@ -5,19 +5,21 @@ from selenium.webdriver.support import expected_conditions as EC
 import os
 
 DEFAULT_TIMEOUT = 10  # time in seconds for waits
-wait = WebDriverWait(driver, DEFAULT_TIMEOUT)
 
 # checks whether element is visible
-def wait_for_visibility(driver, element):
-   wait.until(EC.visibility_of_element_located((element)))
+def wait_for_visibility(driver, element, t = DEFAULT_TIMEOUT):
+    wait = WebDriverWait(driver, DEFAULT_TIMEOUT)
+    return wait.until(EC.visibility_of_element_located((element)))
 
 # checks whether element is clickable
-def wait_for_clickable(driver, element):
-    e = wait.until(EC.element_to_be_clickable((element)))
+def wait_for_clickable(driver, element, t = DEFAULT_TIMEOUT):
+    wait = WebDriverWait(driver, DEFAULT_TIMEOUT)
+    return wait.until(EC.element_to_be_clickable((element)))
 
 # checks whether element is present only in HTML structure
-def wait_for_presence(driver, element):
-    e = wait.until(EC.presence_of_element_located((element)))
+def wait_for_presence(driver, element, t = DEFAULT_TIMEOUT):
+    wait = WebDriverWait(driver, DEFAULT_TIMEOUT)
+    return wait.until(EC.presence_of_element_located((element)))
 
 
 # Function for screenshot
