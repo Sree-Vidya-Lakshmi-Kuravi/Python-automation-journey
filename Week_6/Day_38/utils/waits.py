5771581f51410f2824ec68d1ee997c84f76f7e43
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-21 18:01:57 +0530

    Updated clickable wait

--- Week_6/Day_38/utils/waits.py
+++ Week_6/Day_38/utils/waits.py
@@ -12,7 +12,7 @@ class WaitUtils:
 
     def wait_for_clickable(self, locator):
         element = self.wait.until(EC.element_to_be_clickable((locator)))
-        element.click()
+        return element
 
     def wait_for_presence(self, locator):
         element = self.wait.until(EC.presence_of_element_located((locator)))
