4af74d8c444f8c8292b8e94cbf6d617346a4e0ea
Kuravi Sree Vidya Lakshmi <vidyakuravi@gmail.com>
2026-07-23 16:46:56 +0530

    Created chrome or edge driver instances

---
+++ Week_6/Day_40/drivers/driver_factory.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from config.config_reader import *
+
+class DriverFactory:
+    @staticmethod
+    def get_driver():
+        """Creating browser instance"""
+        browser_name = ConfigReader.get_browser().lower().strip()
+
+        if browser_name == "chrome":
+            driver = webdriver.Chrome()
+
+        elif browser_name == "edge":
+            driver = webdriver.Edge()
+
+        else:
+            raise ValueError(f"Unsupported browser: {browser_name}\nSupported browsers are: chrome, edge")
+          
+        """Maximizing the window"""
+        driver.maximize_window()
+
+        """Applying waits"""
+        driver.implicitly_wait(ConfigReader.get_implicit_wait()
+
+        driver.get(ConfigReader.get_base_url()) 
+        return driver
