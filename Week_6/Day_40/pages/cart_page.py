60dc6acaf8ce45886ab4b5af874d452d8c3cb134
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-24 17:14:30 +0530

    Improved readability and maintainability using logging

--- Week_6/Day_40/pages/cart_page.py
+++ Week_6/Day_40/pages/cart_page.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.common.by import By
 from utils.waits import *
 from pages.base_page import BasePage
+from utils.logger import CustomLogger
+
+logger = CustomLogger.get_logger()
 
 class CartPage(BasePage):
     """Page object for managing shopping cart"""
@@ -14,18 +17,24 @@ class CartPage(BasePage):
 
     def add_product(self):
         """Clicks 'Add to Cart' button"""
+        logger.info("Adding the product to cart...")
         self.click(self.add_to_cart_btn)
 
     def remove_product(self):
         """Clicks 'Remove from Cart' button"""
+        logger.info("Removing the product from cart..")
         self.click(self.remove_from_cart_btn)
 
     def go_to_cart(self):
         """Navigates the cart page by clicking the shopping cart icon"""
+        logger.info("Navigating to the cart icon..")
         self.click(self.cart_icon)
 
     def get_cart_count(self):
         """Returns the current number of items displayed on the cart badge as an integer"""
         if self.is_displayed(self.cart_badge):
-            return int(self.get_text(self.cart_badge))
+            count = int(self.get_text(self.cart_badge))
+            logger.info(f"Current cart count: {count}")
+            return count
+        logger.info("Cart badge displays 0 as count")
         return 0
