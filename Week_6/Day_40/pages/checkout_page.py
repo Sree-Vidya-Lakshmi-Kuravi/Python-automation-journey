0d40fc5d55ddf5135c86a3e626b676924f5c9450
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-24 17:14:52 +0530

    Improved readability and maintainability using logging

--- Week_6/Day_40/pages/checkout_page.py
+++ Week_6/Day_40/pages/checkout_page.py
@@ -1,6 +1,9 @@
 from selenium.webdriver.common.by import By
 from utils.waits import *
 from pages.base_page import BasePage
+from utils.logger import CustomLogger
+
+logger = CustomLogger.get_logger()
 
 class CheckoutPage(BasePage):
     checkout_btn = (By.ID, "checkout")
@@ -15,23 +18,30 @@ class CheckoutPage(BasePage):
         super().__init__(driver)
 
     def click_checkout(self):
+        logger.info("Clicking checkout button..")
         self.click(self.checkout_btn)
 
     def enter_first_name(self, f_name):
+        logger.info(f"Entering first name: {f_name}")
         self.type_text(self.firstname, f_name)
 
     def enter_last_name(self, l_name):
+        logger.info(f"Entering last name: {l_name}")
         self.type_text(self.lastname, l_name)
 
     def enter_zip_code(self, z_code):
+        logger.info(f"Entering zip code: {z_code}")
         self.type_text(self.zipcode, z_code)
 
     def continue_checkout(self):
+        logger.info("Clicking continue button..")
         self.click(self.continue_btn)
 
     def finish_checkout(self):
+        logger.info("Clicking finish button..")
         self.click(self.finish_btn)
 
     def get_thank_you_msg(self):
-        return self.get_text(self.complete_header)
-
+        tq_msg = self.get_text(self.complete_header)
+        logger.info(f"Order status: {tq_msg}")
+        return tq_msg
