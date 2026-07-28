05c1b57e197fbb9937bdf9138cb2a626d9afea31
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-07-28 17:07:12 +0530

    add: implement additional test for negative user creation scenario

--- Week_7/Day_45/tests/test_post_user.py
+++ Week_7/Day_45/tests/test_post_user.py
@@ -8,3 +8,11 @@ def test_post_user():
     }
     response = post("users", data = data)
     assert response.status_code == 201, f"Expected status code 201, but got {response.status_code}"
+
+
+def test_negative_post_user():
+    data = {
+        "email": None
+    }
+    response = post("users/1", data = data)
+    assert response.status_code == 400, f"Expected status code 400, but got {response.status_code}"
