bb980177d5c579d0a85f2945d79575ace51ae5be
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-08-06 16:51:03 +0530

    add: create SQL queries to check for null and not null email and city values

---
+++ MySQL/Week_9/Day_58/null_not_null_probs.sql
@@ -0,0 +1,4 @@
+SELECT email FROM employees WHERE email IS NULL;
+SELECT city FROM employees WHERE city IS NULL;
+SELECT email FROM employees WHERE email IS NOT NULL;
+SELECT city FROM employees WHERE city IS NOT NULL;
