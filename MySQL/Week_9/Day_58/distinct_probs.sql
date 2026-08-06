e1fc306f5690a270e312f401ea9baf20fce4c733
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-08-06 16:49:21 +0530

    add: create SQL queries to retrieve distinct employee attributes

---
+++ MySQL/Week_9/Day_58/distinct_probs.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT dept FROM employees;
+SELECT DISTINCT city FROM employees;
+SELECT DISTINCT designation FROM employees;
+SELECT DISTINCT salary FROM employees;
+SELECT DISTINCT dept, city FROM employees;
