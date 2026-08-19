4f0a13260d926c7937a89a198d2f54eb53f95f4e
Sree-Vidya-Lakshmi <vidyakuravi@gmail.com>
2026-08-19 16:54:08 +0530

    add: create initial database schema with departments, employees, projects, employee_projects tables

---
+++ MySQL/Week_10/Day_70/tables.sql
@@ -0,0 +1,41 @@
+DROP TABLE IF EXISTS accounts;
+DROP TABLE IF EXISTS emp;
+DROP TABLE IF EXISTS employee_projects;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS project_assign;
+
+-- Creating the new tables
+CREATE TABLE departments (
+    department_id INT PRIMARY KEY,
+    department_name VARCHAR(50) NOT NULL,
+    manager_id INT
+);
+
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    job_title VARCHAR(50),
+    salary DECIMAL(10, 2),
+    city VARCHAR(50),
+    joining_date DATE,
+    department_id INT,
+    manager_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+CREATE TABLE projects (
+    project_id INT PRIMARY KEY,
+    project_name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE employee_projects (
+    employee_id INT,
+    project_id INT,
+    PRIMARY KEY (employee_id, project_id),
+    FOREIGN KEY (employee_id) REFERENCES employees(employee_id),
+    FOREIGN KEY (project_id) REFERENCES projects(project_id)
+);
