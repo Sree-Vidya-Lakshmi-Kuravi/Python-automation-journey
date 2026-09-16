02a0eb88f61c413bdb571c3bf728986705cc0780
Kuravi Sree Vidya Lakshmi <vidyakuravi@gmail.com>
2026-09-16 21:28:07 +0530

    Mask database password in validations.py
    Updated the database password to a secure placeholder.

--- MySQL/Week_10/Day_71/validations.py
+++ MySQL/Week_10/Day_71/validations.py
@@ -3,7 +3,7 @@ from mysql.connector import connect
 conn = connect(
         host = "localhost",
         user = "root",
-        password = "siri",
+        password = "****",
         database = "mydb")
 
 cur = conn.cursor()
